06e21e7f432b7b83e4d8603d3269b55b16e8b06b
RiFi2k <luckylowery@gmail.com>
2019-07-10 18:13:15 -0400

    Revert execSync changes
    Figured out this is an issue with the file existing in the docker container

--- src/wordpress.js
+++ src/wordpress.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk')
-const exec = require('child_process').exec
+const execSync = require('child_process').execSync
 const utils = require('./util/utilities')
 const { cacheVolume } = require('./util/variables')
 const log = console.log
@@ -9,16 +9,16 @@ async function download (env) {
   let envPath = await utils.envPath(env)
 
   log(info('Downloading WordPress'))
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core download --force"`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core download --force"`, { stdio: 'inherit', cwd: envPath })
 }
 
 async function downloadDevelop (env) {
   let envPath = await utils.envPath(env)
 
   log(info('Downloading WordPress Develop'))
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "git clone git://develop.git.wordpress.org/ ."`, { cwd: envPath })
-  exec(`docker run -t --rm -v ${envPath}/wordpress:/usr/src/app -v ${cacheVolume}:/var/www/.npm 45air/wpcorebuild:latest npm install`, { cwd: envPath })
-  exec(`docker run -t --rm -v ${envPath}/wordpress:/usr/src/app 45air/wpcorebuild:latest grunt`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "git clone git://develop.git.wordpress.org/ ."`, { stdio: 'inherit', cwd: envPath })
+  execSync(`docker run -t --rm -v ${envPath}/wordpress:/usr/src/app -v ${cacheVolume}:/var/www/.npm 45air/wpcorebuild:latest npm install`, { stdio: 'inherit', cwd: envPath })
+  execSync(`docker run -t --rm -v ${envPath}/wordpress:/usr/src/app 45air/wpcorebuild:latest grunt`, { stdio: 'inherit', cwd: envPath })
 }
 
 async function configure (env) {
@@ -26,7 +26,7 @@ async function configure (env) {
   let envPath = await utils.envPath(env)
 
   log(info('Configuring WordPress'))
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp config create --force --dbname=${envSlug}"`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp config create --force --dbname=${envSlug}"`, { stdio: 'inherit', cwd: envPath })
 }
 
 async function install (env, envHost, answers) {
@@ -50,22 +50,22 @@ async function install (env, envHost, answers) {
       throw Error('Invalid Installation Type')
   }
 
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core ${command} ${flags} --url=http://${envHost} --title=\\"${answers.title}\\" --admin_user=\\"${answers.username}\\" --admin_password=\\"${answers.password}\\" --admin_email=\\"${answers.email}\\""`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp core ${command} ${flags} --url=http://${envHost} --title=\\"${answers.title}\\" --admin_user=\\"${answers.username}\\" --admin_password=\\"${answers.password}\\" --admin_email=\\"${answers.email}\\""`, { stdio: 'inherit', cwd: envPath })
 }
 
 async function setRewrites (env) {
   let envPath = await utils.envPath(env)
 
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp rewrite structure /%postname%/"`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp rewrite structure /%postname%/"`, { stdio: 'inherit', cwd: envPath })
 }
 
 async function emptyContent (env) {
   let envPath = await utils.envPath(env)
 
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp site empty --yes"`, { cwd: envPath })
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp plugin delete hello akismet"`, { cwd: envPath })
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp theme delete twentyfifteen twentysixteen"`, { cwd: envPath })
-  exec(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp widget delete search-2 recent-posts-2 recent-comments-2 archives-2 categories-2 meta-2"`, { cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp site empty --yes"`, { stdio: 'inherit', cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp plugin delete hello akismet"`, { stdio: 'inherit', cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp theme delete twentyfifteen twentysixteen"`, { stdio: 'inherit', cwd: envPath })
+  execSync(`docker-compose exec phpfpm su -s /bin/bash www-data -c "wp widget delete search-2 recent-posts-2 recent-comments-2 archives-2 categories-2 meta-2"`, { stdio: 'inherit', cwd: envPath })
 }
 
 module.exports = { download, downloadDevelop, configure, install, setRewrites, emptyContent }
