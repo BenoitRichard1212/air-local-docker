b0b7ec1d8ceeef40dfa894899594a96df18211ac
RiFi2k <luckylowery@gmail.com>
2019-07-25 15:01:27 -0400

    Revert changes to wp.js

--- src/wp.js
+++ src/wp.js
@@ -1,21 +1,19 @@
 const execSync = require('child_process').execSync
 const gateway = require('./gateway')
 const environment = require('./environment')
-const utils = require('./util/utilities')
-const envUtils = require('./util/env')
 
 const command = async function () {
-  let envSlug = await envUtils.parseOrPromptEnv()
+  const envSlug = await envUtils.parseOrPromptEnv()
   if (envSlug === false) {
     console.error("Error: Unable to determine which environment to use WP CLI with. Please run this command from within your environment's directory.")
     process.exit(1)
   }
 
-  let envPath = await utils.envPath(envSlug)
+  const envPath = await envUtils.envPath(envSlug)
 
   // Check if the container is running, otherwise, start up the stacks
   try {
-    let output = execSync(`docker-compose ps`, { cwd: envPath }).toString()
+    const output = execSync(`docker-compose ps`, { cwd: envPath }).toString()
     if (output.indexOf('phpfpm') === -1) {
       await gateway.startGlobal()
       await environment.start(envSlug)
@@ -23,14 +21,14 @@ const command = async function () {
   } catch (ex) {}
 
   // Get everything after the wp command, so we can pass to the docker container
-  // let command = commandUtils.commandArgs()
+  const command = commandUtils.commandArgs()
 
   // Check for TTY
-  let ttyFlag = process.stdin.isTTY ? '' : '-T'
+  const ttyFlag = process.stdin.isTTY ? '' : '-T'
 
   // Run the command
   try {
-    execSync(`docker-compose exec ${ttyFlag} --user www-data phpfpm wp`, { stdio: 'inherit', cwd: envPath })
+    execSync(`docker-compose exec ${ttyFlag} --user www-data phpfpm wp ${command}`, { stdio: 'inherit', cwd: envPath })
   } catch (ex) {}
 
   process.exit()
